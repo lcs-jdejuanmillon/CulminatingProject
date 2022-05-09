221de1e43fdc1d4faf0cd31f1fd8487e63189fc0
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2022-05-08 21:27:38 -0400

    Initial Commit

---
+++ CulminatingProject/CulminatingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CulminatingProjectApp.swift
+//  CulminatingProject
+//
+//  Created by Jacobo de Juan Millon on 2022-05-08.
+//
+
+import SwiftUI
+
+@main
+struct CulminatingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
