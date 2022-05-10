fc235fc408820f0176a2aadc9034c3aac59deb1e
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2022-05-09 20:16:30 -0400

    Added values and strings for some basic units.

--- CulminatingProject/UnitsAndTypes.swift
+++ CulminatingProject/UnitsAndTypes.swift
@@ -8,3 +8,22 @@
 import Foundation
 
 let types = ["Time", "Displacement", "Initial Velocity", "Final Velocity", "Acceleration"]
+
+struct unit {
+    let text: String
+    let value: Double
+}
+let unitText = [
+    ["s", "min", "h"],
+    ["m", "km", "cm", "mm"],
+    ["m/s", "km/h"],
+    ["m/s", "km/h"],
+    ["m/s^2"]
+]
+let unitValues = [
+    [1, 60, 3600],
+    [1, 1000, 0.01, 0.001],
+    [1, 10/36],
+    [1, 10/36],
+    [1]
+]
